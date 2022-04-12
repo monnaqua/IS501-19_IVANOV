519068ebe902cc679c658fbabeccb782bddedc2b
monnaqua <103503972+monnaqua@users.noreply.github.com>
2022-04-12 14:21:46 +0300

    Add files via upload

---
+++ Game/src/game/Game.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package game;
+
+
+// Ivanov Vladislav Andreevich
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+/**
+ *
+ * @author Студент МПТ
+ */
+// Ivanov Vladislav Andreevich
+public class Game extends JFrame { 
+    
+    private static Game game_window;
+    private static long last_frame_time;
+    private static Image background;
+    private static Image game_over; 
+    private static Image drop;
+    private static float drop_left = 200;
+    private static float drop_top = -100;
+    private static float drop_v = 200;
+    private static int score = 0; 
+    // Ivanov Vladislav Andreevich
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException { 
+    
+     // Ivanov Vladislav Andreevich   
+        background = ImageIO.read(Game.class.getResourceAsStream("background.jpg")); 
+        game_over = ImageIO.read(Game.class.getResourceAsStream("end.png"));
+        drop = ImageIO.read(Game.class.getResourceAsStream("object.png"));
+        
+        game_window = new Game(); 
+        game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        game_window.setLocation(200, 50); 
+        game_window.setSize(900, 600);
+        game_window.setResizable(false); 
+        last_frame_time = System.nanoTime();
+        GameField game_field = new GameField(); 
+        game_field.addMouseListener(new MouseAdapter() {  // Ivanov Vladislav Andreevich
+        @Override
+        public void mousePressed(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        float drop_right = drop_left + drop.getWidth(null);
+        float drop_bottom = drop_top + drop.getHeight(null);
+        boolean is_drop = x >= drop_left && x <= drop_right && y >= drop_top && y <= drop_bottom;
+       
+        if (is_drop) {
+         drop_top = -100; 
+         drop_left = (int) (Math.random() * (game_field.getWidth() - drop.getWidth(null))); // Ivanov Vladislav Andreevich
+         drop_v = drop_v + 10; 
+         score++; 
+         game_window.setTitle("Score: " + score);
+         }
+         }
+        });
+        game_window.add(game_field); // Ivanov Vladislav Andreevich
+        game_window.setVisible(true);
+        
+        
+    }
+    // Ivanov Vladislav Andreevich
+    private static void onRepaint(Graphics g){
+        long current_time = System.nanoTime();
+        float delt_time = (current_time - last_frame_time) * 0.000000001f;
+        last_frame_time = current_time;
+        
+        drop_top = drop_top + drop_v * delt_time;
+        g.drawImage(background, 0, 0, null); 
+        g.drawImage(drop, (int) drop_left, (int) drop_top, null); 
+        if(drop_top > game_window.getHeight()) g.drawImage(game_over, 110, 110, null); 
+    }
+    // Ivanov Vladislav Andreevich
+    private static class GameField extends JPanel { // Ivanov Vladislav Andreevich
+        
+        @Override
+        protected void paintComponent(Graphics g) {// Ivanov Vladislav Andreevich
+            super.paintComponent(g);
+            onRepaint(g);
+            repaint(); 
+        }
+    }
+    
+}
+  
+// Ivanov Vladislav Andreevich
